9e9c2e84dbfd4cce27613ea9a0a0dd124d1a92f1
manoti3 <hillarymanoti0@gmail.com>
2024-03-16 20:27:30 +0300

    completed assignment

--- lib/testing/user_test.py
+++ lib/testing/user_test.py
@@ -2,8 +2,14 @@
 
 from user import User
 
-class TestUser:
-    '''Class "User" in user.py'''
+# class TestUser:
+#     '''Class "User" in user.py'''
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    
 
     def test_is_class(self):
         '''is a class.'''
